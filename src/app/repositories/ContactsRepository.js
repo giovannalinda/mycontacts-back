2e8017bd5673477f47ddf07d0dc591011599d639
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-03 20:18:28 -0300

    Refactoring the delete method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Giovanna',
-    email: 'eugiovannasouza@gmail.com',
-    phone: '999999999',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Gabriel',
-    email: 'eugabriel@gmail.com',
-    phone: '999999999',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -60,11 +41,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
