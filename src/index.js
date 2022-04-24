0a7e2f055cbdad2a408364842a83e142a6df2a14
Giovanna Linda <eugiovannasouza@gmail.com>
2022-04-24 01:09:02 -0300

    caching preflight result

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3001, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('🔥 Server started at http://localhost:3001'));
